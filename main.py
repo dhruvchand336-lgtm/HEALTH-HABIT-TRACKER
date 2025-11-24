2ce5a25de37a066ed79b321d64d438123ee63b23
dhruvchand336-lgtm <dhruvchand336@gmail.com>
2025-11-24 15:30:51 +0530

    main.py

--- main.py
+++ main.py
@@ -1,20 +1,4 @@
-import json
-import os
 from datetime import datetime, timedelta
-
-file_path = "data/habits.json"
-
-if not os.path.exists("data"):
-    os.makedirs("data")
-
-if not os.path.exists(file_path) or os.path.getsize(file_path) == 0:
-    with open(file_path, "w") as f:
-        json.dump({}, f)
-
-with open(file_path, "r") as f:
-    data = json.load(f)
-
-today = datetime.now().strftime("%Y-%m-%d")
 
 print("\n--- Daily Health Habit Tracker ---\n")
 
@@ -23,15 +7,14 @@ sleep = int(input("Sleep (hours): "))
 steps = int(input("Steps walked: "))
 exercise = int(input("Exercise minutes: "))
 
+today = datetime.now().strftime("%Y-%m-%d")
+
 data[today] = {
     "water": water,
     "sleep": sleep,
     "steps": steps,
     "exercise": exercise
 }
-
-with open(file_path, "w") as f:
-    json.dump(data, f, indent=4)
 
 print("\n✔ Entry saved!\n")
 
@@ -41,15 +24,15 @@ total = {"water": 0, "sleep": 0, "steps": 0, "exercise": 0}
 days_count = 0
 
 for i in range(7):
-    date_chk = (datetime.now() - timedelta(days=i)).strftime("%Y-%m-%d")
-    
-    if date_chk in data:
-        total["water"] += data[date_chk]["water"]
-        total["sleep"] += data[date_chk]["sleep"]
-        total["steps"] += data[date_chk]["steps"]
-        total["exercise"] += data[date_chk]["exercise"]
+    date_check = (datetime.now() - timedelta(days=i)).strftime("%Y-%m-%d")
+
+    if date_check in data:
+        total["water"] += data[date_check]["water"]
+        total["sleep"] += data[date_check]["sleep"]
+        total["steps"] += data[date_check]["steps"]
+        total["exercise"] += data[date_check]["exercise"]
         days_count += 1
-        
+
 if days_count > 0:
     print("Average Water     :", total["water"] // days_count)
     print("Average Sleep     :", total["sleep"] // days_count)
